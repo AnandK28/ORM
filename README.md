536dd9ee5639942480aa3a87ec154481ba2f8cbf
AnandK28 <kanand2811@gmail.com>
2024-10-27 22:40:31 +0530

    Success

--- README.md
+++ README.md
@@ -31,7 +31,8 @@ from django.contrib import adming
 class Bank(models.Model):
 	Name=models.CharField(max_length=20)
 	AccNo=models.IntegerField(primary_key="AccNo")
-	LoanAmt=models.IntegerField()
+	PhoneNo=models.Integerfield()
+    LoanAmt=models.IntegerField()
 	Interest=models.IntegerField()
 class BankAdmin(admin.ModelAdmin):
 	list_display=('Name','AccNo','LoanAmt','Interest')
--- connor/detroit/models.py
+++ connor/detroit/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from django.contrib import adming
+from django.contrib import admin
 class Bank(models.Model):
 	Name=models.CharField(max_length=20)
 	AccNo=models.IntegerField(primary_key="AccNo")
+	PhoneNo=models.IntegerField(max_length=10)
 	LoanAmt=models.IntegerField()
 	Interest=models.IntegerField()
 class BankAdmin(admin.ModelAdmin):
-	list_display=('Name','AccNo','LoanAmt','Interest')
-	
+	list_display=('Name','AccNo','PhoneNo','LoanAmt','Interest')
